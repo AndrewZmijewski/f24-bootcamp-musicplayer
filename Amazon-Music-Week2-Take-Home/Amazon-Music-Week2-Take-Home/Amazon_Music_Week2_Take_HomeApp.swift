81392a71446ed9066325dfeb5c6bf27440ca5b67
AndrewZmijewski <andrewvz@ad.unc.edu>
2024-09-24 18:34:55 -0400

    Amazon Music

---
+++ Amazon-Music-Week2-Take-Home/Amazon-Music-Week2-Take-Home/Amazon_Music_Week2_Take_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amazon_Music_Week2_Take_HomeApp.swift
+//  Amazon-Music-Week2-Take-Home
+//
+//  Created by Andrew Zmijewski on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Amazon_Music_Week2_Take_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
